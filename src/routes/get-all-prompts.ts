c363c1e27a11f6855c7d7edfd9f244d2747979c6
DiegoCard117 <diegocardoso117@gmail.com>
2023-09-13 14:11:50 -0300

    Backend feito

--- src/routes/get-all-prompts.ts
+++ src/routes/get-all-prompts.ts
@@ -3,7 +3,7 @@ import { prisma } from "../lib/prisma";
 
 export async function getAllPromptsRoutes(app: FastifyInstance) {
   app.get('/prompts', async () => {
-    const prompts = await prisma.prompt.findMany()
-    return prompts
-  })
+    const prompts = await prisma.prompt.findMany();
+    return prompts;
+  });
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,24 @@
-import { fastify } from 'fastify'
+import { fastify } from 'fastify';
 import { getAllPromptsRoutes } from './routes/get-all-prompts';
+import { uploadVideoRoute } from './routes/upload-video';
+import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 
-const app = fastify()
+import { fastifyCors } from '@fastify/cors'
 
-app.register(getAllPromptsRoutes)
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: '*',
+})
+
+app.register(getAllPromptsRoutes);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAiCompletionRoute)
 
 app.listen({
   port: 3333
-}).then(() =>{
-  console.log('Http server running')
-})
+}).then(() => {
+  console.log('Http server running');
+});
